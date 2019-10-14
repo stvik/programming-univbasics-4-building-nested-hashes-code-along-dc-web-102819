68b895b6992ed18b67a1f028a95d828e9004bb00
Sara Tarnvik <stvik2@gmail.com>
2019-10-14 19:58:18 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -3,11 +3,16 @@ def second_challenge
     :montague => {
       #your key/value pairs here!
       patriarch: {},
-      matriarch: {}
-      
+      matriarch: {},
+      hero: {},
+      hero_friends: []
       }, 
     :capulet => {
       #your key/value pairs here!
+      patriarch: {},
+      matriarch: {},
+      heroine: {},
+      heroine_friends: []
       }}
 
   
